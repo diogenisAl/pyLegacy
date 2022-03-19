e0b25d1db9cb9955fe7f061455c1b7dd85fd6dbc
diogenisAl <diogenis_al@yahoo.com>
2022-03-19 22:40:46 +0200

    1st demo upload

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/") #this is the home page
-def home():
-  return "<h1>yo!</h1>"
 
-if __name__ == "__main__":
-  app.run()
+@app.route('/')
+def yolo():
+
+    # the initial dictionary with members' attributes
+    legacy_dictionary = {"20eeee": {"youtube_url": "https://di.ionio.gr", "source_code_url": "https://di.ionio.gr"} };
+
+    return jsonify(legacy_dictionary)
+
+
+if __name__ == '__main__':
+    app.run()
